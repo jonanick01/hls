b77b4d3a3d663e833331cb768db8287c6deeed99
jonanick01 <81921413+jonanick01@users.noreply.github.com>
2022-04-02 11:36:43 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 # Change input and output details
 
-output_path="thDB/romantic" # Output Path (Same will be pushed in main also)
+output_path="thDB/spider" # Output Path (Same will be pushed in main also)
 input_url="https://index.jonatv.workers.dev/0:/Movies/Spider-Man.No.Way.Home.mkv" # Input direct file url
 input_extension="mkv" # Extension of file url
 
@@ -12,12 +12,12 @@ wget --quiet -O video.$input_extension $input_url
 mkdir $output_path
 
 ffmpeg -hide_banner -y -i video.$input_extension \
-  -vf scale=w=1960:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 192k -hls_segment_filename $output_path/1080p_%03d.ts $output_path/1080p.m3u8 
+  -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename $output_path/480p_%03d.ts $output_path/480p.m3u8 
 
 rm video.$input_extension
 cd $output_path
 
 echo '#EXTM3U
 #EXT-X-VERSION:3
-#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=1960x1080
-1080p.m3u8' > master.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480
+480p.m3u8' > master.m3u8
