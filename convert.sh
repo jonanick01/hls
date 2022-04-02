e042b76340b9cbbd0bc9c0b056be79047e55e0ee
jonanick01 <81921413+jonanick01@users.noreply.github.com>
2022-04-02 16:49:49 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -8,6 +8,9 @@ input_extension="mkv" # Extension of file url
 
 # Change ffmpeg configurations according to yur need (If you don't know, don't touch)
 
+wget --quiet -O video.$input_extension $input_url
+mkdir $output_path
+
 ffmpeg -hide_banner -y -i video.$input_extension \
   -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename $output_path/360p_%03d.ts $output_path/360p.m3u8 \
   -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename $output_path/480p_%03d.ts $output_path/480p.m3u8 \
